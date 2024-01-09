4ac670bf46d598bffa4e44a9a55b906d156ad694
Kibryant <arthurgustavon@gmail.com>
2024-01-09 14:17:49 -0300

    feat(ui): adding possibility of changing theme

---
+++ src/components/Providers/index.tsx
@@ -0,0 +1,48 @@
+'use client'
+
+import { ThemeProvider } from 'next-themes'
+import { Alert, AlertTitle, AlertDescription } from '../ui/alert'
+import { useState, useEffect } from 'react'
+import { RocketIcon } from 'lucide-react'
+
+const Providers = ({ children }: { children: React.ReactNode }) => {
+    const [showAlert, setShowAlert] = useState(false)
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentScrollY = window.scrollY
+            if (currentScrollY > 100) {
+                setShowAlert(true)
+            } else {
+                setShowAlert(false)
+            }
+        }
+
+        window.addEventListener('scroll', handleScroll)
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
+    }, [])
+
+    return (
+        <>
+                <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+                    {children}
+                </ThemeProvider>
+
+                {showAlert && (
+                    <Alert className="fixed bottom-2 right-2 max-w-sm alert">
+                        <RocketIcon className="h-4 w-4" />
+                        <AlertTitle>Heads up!</AlertTitle>
+                        <AlertDescription>
+                            You can add components and dependencies to your app using the cli.
+                        </AlertDescription>
+                    </Alert>
+                )}
+
+        </>
+    )
+}
+
+export { Providers }
