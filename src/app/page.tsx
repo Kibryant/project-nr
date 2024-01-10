dd4a44235ec51516de1b6396ddbf21e6ed45b809
Kibryant <arthurgustavon@gmail.com>
2024-01-10 11:57:09 -0300

    styles: adding content to the header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,13 @@ import { Header } from "@/components/Header";
 const Page = () => {
     return (
         <>
-            <Header />
+            <Header
+                title="Seja bem vindo(a) á"
+                titleGreenPart="NR Representações!"
+                subTitle="Nossa história é de paixão por qualidade e dedicação ao cliente."
+                subtitleGreenPart="Desde de 2018, fornecemos os melhores produtos para todo o Nordeste!"
+                showButtons
+            />
         </>
     );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,42 @@
-const Header = () => {
+import { Button } from "../ui/button"
+
+interface HeaderProps {
+    title: string
+    titleGreenPart: string
+    subTitle: string
+    subtitleGreenPart: string
+    showButtons?: boolean
+}
+
+const Header = ({
+    title,
+    subTitle,
+    titleGreenPart,
+    subtitleGreenPart,
+    showButtons = false
+}: HeaderProps) => {
     return (
         <header className="flex flex-col items-center justify-center w-full gap-y-4 sm:gap-6 sm:py-4 px-2">
+            <div>
+                <div className="mx-auto max-w-2xl">
+                    <h1 className="font-bold text-4xl text-center">
+                        {title} <span className="text-primary">{titleGreenPart}</span>
+                    </h1>
+                    <p className="mt-4 text-center">
+                        {subTitle} <span className="text-primary font-semibold">{subtitleGreenPart}</span>
+                    </p>
+                </div>
+            </div>
+            {showButtons && (
+                <div>
+                    <Button className="mt-4">
+                        Entre em contato
+                    </Button>
+                    <Button  className="mt-4">
+                        Conheça nossos produtos
+                    </Button>
+                </div>
+            )}
         </header>
     )
 }
